3e66ff51933bf07011930482e59174b9cec43755
judah.jacobson <judah.jacobson@gmail.com>
2007-09-21 18:43:52 +0000

    More work on the Base module.

--- System/Console/Terminfo/Base.hs
+++ System/Console/Terminfo/Base.hs
@@ -7,8 +7,10 @@ module System.Console.Terminfo.Base(
                             tiGetNum,
                             tiGetStr,
                             tPuts,
+                            LinesAffected,
                             tParm,
                             TParm(..),
+                            TParmInput
                             ) where
 
 import Foreign.C
@@ -22,8 +24,10 @@ import System.IO.Unsafe
 -- TODO: Explicit header import.
 foreign import ccall setupterm :: CString -> CInt -> Ptr CInt -> IO ()
 
--- Initialize the terminfo library to the given terminal entry, 
--- using the system terminfo directory.  
+-- | Initialize the terminfo library to the given terminal entry.
+--
+-- NOTE: Either 'setupTerm' or 'setupTermFromEnv' must be run before any capabilities
+-- are looked up.
 setupTerm :: String -> IO ()
 setupTerm term = withCString term $ \c_term -> 
                     with 0 $ \ret_ptr -> do
@@ -32,49 +36,24 @@ setupTerm term = withCString term $ \c_term ->
                         ret <- peek ret_ptr
                         when (ret /= 1) $ error ("Couldn't lookup terminfo entry " ++ show term)
                             
--- Initialize the terminfo library, using the TERM environmental variable.
--- If TERM is not set, we use the generic entry "dumb".
+-- | Initialize the terminfo library, using the @TERM@ environmental variable.
+-- If @TERM@ is not set, we use the generic, minimal entry @dumb@.
 setupTermFromEnv :: IO ()
 setupTermFromEnv = do
     env_term <- getEnv "TERM" 
     let term = if null env_term then "dumb" else env_term
     setupTerm term
 
-foreign import ccall tparm ::
-    CString -> CLong -> CLong -> CLong -> CLong -> CLong -> CLong 
-    -> CLong -> CLong -> CLong -- p1,...,p9
-    -> IO CString
-
-
-type TParmFunc = 
-    Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> Int -> String
-
-class TParm f where
-    applyTParm :: TParmFunc -> f
-
-instance TParm (Int -> String) where
-    applyTParm f p1 = f p1 0 0 0 0 0 0 0 0
-
-instance TParm (Int -> Int -> String) where
-    applyTParm f p1 p2 = f p1 p2 0 0 0 0 0 0 0
-
--- Note: the purity of this function really depends on the implementation;
--- and this may not be multithreaded-safe.
-tparm_base :: String -> TParmFunc
-tparm_base cap p1 p2 p3 p4 p5 p6 p7 p8 p9 
-    = unsafePerformIO $ withCString cap $ \c_cap -> do
-        result <- tparm c_cap (toEnum p1) (toEnum p2) (toEnum p3) (toEnum p4) 
-                    (toEnum p5) (toEnum p6) (toEnum p7) (toEnum p8) (toEnum p9)
-        peekCString result
-
-tParm :: TParm f => String -> f
-tParm s = applyTParm (tparm_base s)
-
-
-
-
+{-- TODO: maybe???
+data Available a = WrongType | Absent | Present a
+flag : ()
+class Capability a where
+    getCap :: String -> Int (Available a)
+--}
 
 foreign import ccall tigetnum :: CString -> IO CInt
+
+-- | Look up a numeric capability in the terminfo database.
 tiGetNum :: String -> IO (Maybe Int)
 tiGetNum cap = withCString cap $ \c_cap -> do
                 n <- fmap fromEnum (tigetnum c_cap)
@@ -82,42 +61,103 @@ tiGetNum cap = withCString cap $ \c_cap -> do
                     then return (Just n)
                     else return Nothing
 
+foreign import ccall tigetflag :: CString -> IO CInt
+-- | Look up a boolean capability in the terminfo database.
+tiGetFlag :: String -> IO Bool
+tiGetFlag cap = withCString cap $ \c_cap -> 
+                fmap (>0) (tigetflag c_cap)
                 
--- TODO: what if not available
+                
 foreign import ccall tigetstr :: CString -> IO CString
 
+-- | Look up a string capability in the terminfo database.  
+--
+-- Note: All terminfo strings should be printed with 'tPuts'.
 tiGetStr :: String -> IO (Maybe String)
 tiGetStr cap = withCString cap $ \c_cap -> do
                 result <- tigetstr c_cap
                 if result == nullPtr || result == neg1Ptr
                     then return Nothing
                     else fmap Just (peekCString result)
-
--- hack; tigetstr sometimes returns (-1)
-neg1Ptr = nullPtr `plusPtr` (-1)
+    where
+        -- hack; tigetstr sometimes returns (-1)
+        neg1Ptr = nullPtr `plusPtr` (-1)
                     
-foreign import ccall tigetflag :: CString -> IO CInt
-tiGetFlag :: String -> IO Bool
-tiGetFlag cap = withCString cap $ \c_cap -> 
-                fmap (== 1) (tigetflag c_cap)
-                
--- TODO: initialize PC to "pc" capability, or null otherwise.
--- also: PC, ospeed, UP, BC
+foreign import ccall tparm ::
+    CString -> CLong -> CLong -> CLong -> CLong -> CLong -> CLong 
+    -> CLong -> CLong -> CLong -- p1,...,p9
+    -> IO CString
+
+
 -- Note: I may want to cut out the middleman and pipe tGoto/tGetStr together
 -- with tput without a String marshall in the middle.
 -- directly without 
+
+type TParmInput = (Int, Int, Int, Int, Int, Int, Int, Int, Int)
+
+class TParm a where
+    toTParmInput :: a -> TParmInput
+
+instance TParm TParmInput where
+    toTParmInput = id
+
+instance TParm Int where
+    toTParmInput p1 = (p1,0,0,0,0,0,0,0,0)
+
+instance TParm (Int,Int) where
+    toTParmInput (p1,p2) = (p1,p2,0,0,0,0,0,0,0)
+
+instance TParm (Int,Int,Int) where
+    toTParmInput (p1,p2,p3) = (p1,p2,p3,0,0,0,0,0,0)
+
+instance TParm (Int,Int,Int,Int) where
+    toTParmInput (p1,p2,p3,p4) = (p1,p2,p3,p4,0,0,0,0,0)
+
+instance TParm (Int,Int,Int,Int,Int) where
+    toTParmInput (p1,p2,p3,p4,p5) = (p1,p2,p3,p4,p5,0,0,0,0)
+
+instance TParm (Int,Int,Int,Int,Int,Int) where
+    toTParmInput (p1,p2,p3,p4,p5,p6) = (p1,p2,p3,p4,p5,p6,0,0,0)
+
+instance TParm (Int,Int,Int,Int,Int,Int,Int) where
+    toTParmInput (p1,p2,p3,p4,p5,p6,p7) = (p1,p2,p3,p4,p5,p6,p7,0,0)
+
+instance TParm (Int,Int,Int,Int,Int,Int,Int,Int) where
+    toTParmInput (p1,p2,p3,p4,p5,p6,p7,p8) = (p1,p2,p3,p4,p5,p6,p7,p8,0)
+
+
+-- Note: the purity of this function really depends on the implementation;
+-- and this may not be multithreaded-safe.
+tparm_base :: String -> TParmInput -> String
+tparm_base cap (p1,p2,p3,p4,p5,p6,p7,p8,p9)
+    = unsafePerformIO $ withCString cap $ \c_cap -> do
+        result <- tparm c_cap (toEnum p1) (toEnum p2) (toEnum p3) (toEnum p4) 
+                    (toEnum p5) (toEnum p6) (toEnum p7) (toEnum p8) (toEnum p9)
+        peekCString result
+
+-- | Substitute parameters into a string capability.
+--
+-- Note: All terminfo strings should be printed with 'tPuts'.
+tParm :: TParm a => String -> a -> String
+tParm s x = tparm_base s (toTParmInput x)
+
+
 
 type CharOutput = CInt -> IO CInt
 foreign import ccall "wrapper" mkCallback :: CharOutput -> IO (FunPtr CharOutput)
 
 foreign import ccall tputs :: CString -> CInt -> FunPtr CharOutput -> IO ()
 
-tPuts :: String -> Int -> (Char -> IO ()) -> IO ()
+type LinesAffected = Int
+
+-- | Output a string capability.  Applys padding information to the string if
+-- necessary.
+tPuts :: String -> LinesAffected 
+        -> (Char -> IO ()) -- ^ An output function, e.g. 'putChar'.
+        -> IO ()
 tPuts s n f = withCString s $ \c_str -> do
                 fun <- mkCallback (\c -> let c' = toEnum $ fromEnum c 
                                          in f c' >> return c)
                 tputs c_str (toEnum n) fun
                 freeHaskellFunPtr fun
-
-
 
