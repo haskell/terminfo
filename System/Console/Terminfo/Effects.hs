0532b2a9ed7b9ded1222a2562e3e2a37523fa9d9
judah.jacobson <judah.jacobson@gmail.com>
2007-11-25 20:11:24 +0000

    Some basic effect capabilities.

--- System/Console/Terminfo/Effects.hs
+++ System/Console/Terminfo/Effects.hs
@@ -1,3 +1,25 @@
 module System.Console.Terminfo.Effects where
 
 import System.Console.Terminfo.Base
+
+wrapWith :: Capability TermOutput -> Capability TermOutput 
+                -> Capability (TermOutput -> TermOutput)
+wrapWith start end = do
+    s <- start
+    e <- end
+    return (\t -> s `mappend` t `mappend` e)
+
+withStandout, withUnderline :: Capability (TermOutput -> TermOutput)
+withStandout = wrapWith enterStandoutMode exitStandoutMode
+withUnderline = wrapWith enterUnderlineMode exitUnderlineMode
+
+enterStandoutMode, exitStandoutMode :: Capability TermOutput
+enterStandoutMode = tiGetOutput1 "smso"
+exitStandoutMode = tiGetOutput1 "rmso"
+
+enterUnderlineMode, exitUnderlineMode :: Capability TermOutput
+enterUnderlineMode = tiGetOutput1 "smul"
+exitUnderlineMode = tiGetOutput1 "rmul"
+
+
+
