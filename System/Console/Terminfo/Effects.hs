890de265165e20b49c313c3cb791996cd3307915
Judah Jacobson <judah.jacobson@gmail.com>
2009-07-18 17:38:01 +0000

    Add full set of text attributes.

--- System/Console/Terminfo/Effects.hs
+++ System/Console/Terminfo/Effects.hs
@@ -5,21 +5,31 @@
 module System.Console.Terminfo.Effects(
                     -- * Bell alerts
                     bell,visualBell,
-                    -- * Text effect wrappers
+                    -- * Text attributes
+                    Attributes(..),
+                    defaultAttributes,
+                    withAttributes,
+                    setAttributes,
+                    allAttributesOff,
+                    -- ** Mode wrappers
                     withStandout,
                     withUnderline,
                     withBold,
-                    -- * Modes
+                    -- ** Low-level capabilities
                     enterStandoutMode,
                     exitStandoutMode,
                     enterUnderlineMode,
                     exitUnderlineMode,
-                    -- * Attributes
+                    reverseOn,
+                    blinkOn,
                     boldOn,
-                    allAttributesOff
+                    dimOn,
+                    invisibleOn,
+                    protectedOn
                     ) where
 
 import System.Console.Terminfo.Base
+import Control.Monad
 
 wrapWith :: Capability TermOutput -> Capability TermOutput 
                 -> Capability (TermOutput -> TermOutput)
@@ -55,11 +65,90 @@ enterUnderlineMode = tiGetOutput1 "smul"
 exitUnderlineMode :: Capability TermOutput
 exitUnderlineMode = tiGetOutput1 "rmul"
 
+reverseOn :: Capability TermOutput
+reverseOn = tiGetOutput1 "rev"
+
+blinkOn:: Capability TermOutput
+blinkOn = tiGetOutput1 "blink"
+
 boldOn :: Capability TermOutput
 boldOn = tiGetOutput1 "bold"
 
+dimOn :: Capability TermOutput
+dimOn = tiGetOutput1 "dim"
+
+invisibleOn :: Capability TermOutput
+invisibleOn = tiGetOutput1 "invis"
+
+protectedOn :: Capability TermOutput
+protectedOn = tiGetOutput1 "prot"
+
+-- | Turns off all text attributes.  This capability will always succeed, but it has
+-- no effect in terminals which do not support text attributes.
 allAttributesOff :: Capability TermOutput
-allAttributesOff = tiGetOutput1 "sgr0"
+allAttributesOff = tiGetOutput1 "sgr0" `mplus` return mempty
+
+data Attributes = Attributes {
+                    standoutAttr,
+                    underlineAttr,
+                    reverseAttr,
+                    blinkAttr,
+                    dimAttr,
+                    boldAttr,
+                    invisibleAttr,
+                    protectedAttr :: Bool
+                -- NB: I'm not including the "alternate character set." 
+                }
+
+-- | Sets the attributes on or off before outputting the given text,
+-- and then turns them all off.  This capability will always succeed; properties
+-- which cannot be set in the current terminal will be ignored.
+withAttributes :: Capability (Attributes -> TermOutput -> TermOutput)
+withAttributes = do
+    set <- setAttributes
+    off <- allAttributesOff
+    return $ \attrs to -> set attrs <#> to <#> off
+
+-- | Sets the attributes on or off.  This capability will always succeed;
+-- properties which cannot be set in the current terminal will be ignored.
+setAttributes :: Capability (Attributes -> TermOutput)
+setAttributes = usingSGR0 `mplus` manualSets
+    where
+        usingSGR0 = do
+            sgr <- tiGetOutput "sgr"
+            return $ \a -> let mkAttr f = if f a then 1 else 0
+                           in flip sgr 0 $ map mkAttr [ standoutAttr
+                                                       , underlineAttr
+                                                       , reverseAttr
+                                                       , blinkAttr
+                                                       , dimAttr
+                                                       , boldAttr
+                                                       , invisibleAttr
+                                                       , protectedAttr
+                                                       ]
+                                                ++ [0] -- for alt. char sets
+        attrCap :: (Attributes -> Bool) -> Capability TermOutput
+                    -> Capability (Attributes -> TermOutput)
+        attrCap f cap = do {to <- cap; return $ \a -> if f a then to else mempty}
+                        `mplus` return (const mempty)
+        manualSets = do
+            allOff <- allAttributesOff
+            cs <- sequence [attrCap standoutAttr enterStandoutMode
+                            , attrCap underlineAttr enterUnderlineMode
+                            , attrCap reverseAttr reverseOn
+                            , attrCap blinkAttr blinkOn
+                            , attrCap boldAttr boldOn
+                            , attrCap dimAttr dimOn
+                            , attrCap invisibleAttr invisibleOn
+                            , attrCap protectedAttr protectedOn
+                            ]
+            return $ \a -> mconcat $ map ($ a) cs
+
+                                     
+
+-- | These attributes have all properties turned off.
+defaultAttributes :: Attributes
+defaultAttributes = Attributes False False False False False False False False
 
 -- | Sound the audible bell.
 bell :: Capability TermOutput
